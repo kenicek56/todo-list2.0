435104f488cad31cd7472a75fe221921370e930c
kenice washington <kenicek56@gmail.com>
2015-04-23 11:50:02 -0700

    finished video 2

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Kenice's To-Do 2.0 List</title>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+ <div class="wrap">
+	
+ </div>
+</body>
+</html>
