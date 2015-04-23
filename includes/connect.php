e1b04a773d861d02b487c420204364c9003f4e21
kenice washington <kenicek56@gmail.com>
2015-04-23 12:23:19 -0700

    finished video 5

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 if ($mysqli->connect_error) {
 die('Connect Error('. $mysqli->connect_errno . ')'
 	. $mysqli->connect_error);
@@ -7,5 +7,6 @@ die('Connect Error('. $mysqli->connect_errno . ')'
 else{
 	echo "Connection made";
 }
+
 $mysqli->close();
 ?>
